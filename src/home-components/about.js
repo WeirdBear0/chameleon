5b8cadb0eead232154697e9d13e15f0794c2065b
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-05-30 23:29:58 -0700

    response abt

--- src/home-components/about.js
+++ src/home-components/about.js
@@ -5,20 +5,25 @@ const About = () => {
   return (
     <div>
         <div className='aboutContainer'>
-              <div className='team'>
-                <h1 className='title'>Our Team</h1>
-                <p className='desc'>
-                  ---------------------------------------------
-                </p>
-              </div>
-              <span className='statement'>
+              <span className='statementmobile'>
                 <p>"A nation that destroys its soil destroys itself." - Franklin Delano Roosevelt</p>
               </span>
-              <div className='cause'>
-                <h1 className='title'>Our Cause</h1>
+              <div className='webcontainer'>
+                <div className='team'>
+                  <h1 className='title'>Our Team</h1>
                   <p className='desc'>
                     ---------------------------------------------
                   </p>
+                </div>
+                <span className='statementweb'>
+                  <p>"A nation that destroys its soil destroys itself." - Franklin Delano Roosevelt</p>
+                </span>
+                <div className='cause'>
+                  <h1 className='title'>Our Cause</h1>
+                    <p className='desc'>
+                      ---------------------------------------------
+                    </p>
+                </div>
               </div>
             </div>
     </div>
