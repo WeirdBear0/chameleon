91d761a858e922f4e042b274aeedddf676401963
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-05-31 23:01:46 -0700

    abt content

--- src/home-components/about.js
+++ src/home-components/about.js
@@ -9,7 +9,7 @@ const About = () => {
                 <div className='team'>
                   <h1 className='title'>Our Story</h1>
                   <p className='desc'>
-                    In a world flooded with endless entertainment, internet and connection, we realized that there was a lack of genuine
+                    In a world flooded with endless entertainment, internet and social media, we realized that there was a lack of genuine
                     experiences in our community. At Chameleon Camps, we create engaging projects and lessons that are designed 
                     to bring youth into the moment and spark ideas and wonder!
                   </p>
