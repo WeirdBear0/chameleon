a7c50d999e8bf056d53bde158c63370ffdfbd709
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-05-31 17:16:12 -0700

    camps detail info

--- src/camp-pages/ecology.js
+++ src/camp-pages/ecology.js
@@ -6,9 +6,13 @@ function Ecology() {
         <p className='title'>Ecology</p>
         <div className='info-container'>
           <div className='info-width'>
-            <p className='info'>The environment is our shared home, deserving of our utmost care and protection. 
-                                Every action we take, no matter how small, can contribute to preserving its beauty and sustainability 
-                                for future generations.</p>
+            <p className='info'>
+              Our Ecology and Technology camp focuses on the exploration of advanced farming through small-scale class farms integrated 
+              with analytic devices such as the Microsoft FarmBeat. Students will explore this large project throughout the week and will
+              accompany it with a mini-project involving the creation of a terrarium. Additionally, students will take a field trip to
+              Bellevue Botanical Garden to understand and explore various niches in global environments. To finish off, presentations
+              and class discussions will be held to encapsulate the week's learning!
+            </p>
           </div>
           
       </div>
