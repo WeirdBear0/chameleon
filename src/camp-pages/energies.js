b44f02b0d125cc1a0e857fcf04ec741170e3f781
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-05-31 17:46:11 -0700

    camp info

--- src/camp-pages/energies.js
+++ src/camp-pages/energies.js
@@ -3,12 +3,17 @@ import React, {useState, useRef} from 'react';
 function Energies() {
   return (
     <div className='campInfo'>
+        <p className='specs'> July 15-19 | 10-1 PM | $200</p>
         <p className='title'>Energies</p>
         <div className='info-container'>
           <div className='info-width'>
-            <p className='info'>The environment is our shared home, deserving of our utmost care and protection. 
-                                Every action we take, no matter how small, can contribute to preserving its beauty and sustainability 
-                                for future generations.</p>
+            <p className='info'>
+              In our Energies camp, students will learn about, examine, build, and test different methods of renewable energy, namely wind
+              and hydro-power. Students will be shown specialized gearboxes that are used in fundamental structures such as windmills and
+              dams, and will explore mechanisms such as the nacelle turning mechanism, in windmills. Attendees will then generate small
+              amounts of electricity with their own mechanisms, and test them on natural subjects, such as creeks and streams. Finishing off,
+              kids will explore the various environmental benefits of clean energy solutions and learn how to scale what they built over the week!
+            </p>
           </div>
           
       </div>
--- src/camp-pages/water.js
+++ src/camp-pages/water.js
@@ -3,12 +3,18 @@ import React, {useState, useRef} from 'react';
 function Water() {
   return (
     <div className='campInfo'>
+        <p className='specs'> July 1-5 | 10-1 PM | $200</p>
         <p className='title'>Water</p>
         <div className='info-container'>
           <div className='info-width'>
-            <p className='info'>The environment is our shared home, deserving of our utmost care and protection. 
-                                Every action we take, no matter how small, can contribute to preserving its beauty and sustainability 
-                                for future generations.</p>
+            <p className='info'>
+              In our Week of Water, students will begin learning about the water cycle and fundamental concepts that affect how water interacts with
+              the environment around it. As students dive into the project, they will be presented with a diorama featuring a prominent water feature (river, lake, waterfall)
+              and objects such as mountains, villages, and forests. The problem students will aim to solve will be to prevent the village from being destroyed from the 
+              prominent water feature. Concepts regarding engineering and environmental science will be implemented to protect the village. Midweek, students will
+              embark on a community service project, where they will clean up a nearby creek or stream. Finally, real-life examples of the dioramas will be introduced, and
+              case studies will be conducted to make strong connections with the real world. 
+            </p>
           </div>
           
       </div>
