03018b3d436daead4226bbfabbc09a1fe75ba04e
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-06-11 08:04:38 -0700

    update phone

--- src/home-components/footer.js
+++ src/home-components/footer.js
@@ -13,6 +13,7 @@ const Footer = () => {
                                 <p className={styles.title}>CONTACT</p>
                                 <div className={styles.links}>
                                     <a href = "mailto:camps.chameleon@gmail.com" className={styles.mailto}><p>Reach us at <span className={styles.email}>camps.chameleon@gmail.com</span></p></a>
+                                    <p>Call or Text at 425.738.2825</p>
                                 </div>
                             </div>
                             <div className={styles.card}>
