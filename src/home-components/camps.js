73edb6347bed5fc983eee035ffff3d0c13f56a59
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-07-03 19:56:57 -0700

    update for cleanliness

--- src/home-components/camps.js
+++ src/home-components/camps.js
@@ -6,6 +6,7 @@ const Camps = () => {
   return (
     <div className='ovr'>
       <div className='campsContainer'>
+        <div className = "sectionHeader">
           <div className='title'>
               <p className='campsHeader'>
                   <span className='plain'>The secrets of nature <br/></span>
@@ -13,29 +14,16 @@ const Camps = () => {
               </p>
           </div>
           <p className='cohort-header'>SUMMER CAMPS - 2024</p>
+        </div>
           <div className='campCards'>
             <Link to = "/" className='card'>
                 <p className='card-title'>Week 1: Week Of Water</p>
-                <p className='desc'>
-                  The most necessary thing for life to exist: water. This week focuses on how water interacts with the environment, planet,
-                 and people, and how a better understanding of water can lead to a more sustainable and efficient society.
-                </p>
             </Link>
             <Link to = "/energies" className='card'>
                 <p className='card-title'>Week 2: Clean Energies</p>
-                <p className='desc'>
-                  Clean energy becomes more prominent day by day, showing through forms such as wind, solar, and hydropower energy. 
-                  Learning how these systems work and about the significance of their engineering will open minds to their endless 
-                  possibilities!
-                </p>
             </Link>
             <Link to = "/ecology" className='card'>
                 <p className='card-title'>Week 3: Ecology and Technology</p>
-                <p className='desc'>
-                    With technology rapidly expanding in our modern world, many of us feel as if the concept of ecology is pointless. In
-                    reality, with the help of technological integration into ecology, the hybrid between natural learning and STEM will
-                    carry on into the future.
-                </p>
             </Link>
           </div>
       </div>  
