0493e3d0a495c09a92af2216ac06898ce71beebf
weirdbear0 <ayushagarwalsamm@gmail.com>
2024-10-11 12:12:21 -0700

    push liknk

--- src/home-components/banner.js
+++ src/home-components/banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <p className='title'>A Breath of Fresh Air</p>
         <p className='description'>Innovative camps with hands-on learning</p>
         <p className='specs'> EST. 2024 | Sammamish, WA </p>
-        <p><a href = "https://docs.google.com/forms/d/e/1FAIpQLScxWFGRNo3n2V6pBgutWGnjz5vggMSH0j4fvQf2y49_Y2vIgA/viewform" style = {{textDecoration: 'none', color: "#6c584c", fontFamily : 'Montserrat'}}>Interested? Let us know!</a></p>
+        <p><a href = "https://docs.google.com/forms/d/e/1FAIpQLScxWFGRNo3n2V6pBgutWGnjz5vggMSH0j4fvQf2y49_Y2vIgA/viewform" style = {{color: "#6c584c", fontFamily : 'Montserrat'}} target='_blank'>Interested? Let us know!</a></p>
       </div>
     </div>
   );
