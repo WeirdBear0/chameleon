3c15668d6a92c0c4aa859d22b35c0d72ccbd1a2e
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-01-10 11:13:50 -0800

    remove vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useRef} from 'react';
-import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Home'
 import Water from './camp-pages/water'
@@ -11,7 +10,6 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-      <Analytics/>
       <Routes>
         <Route path = "/" element = {<Home/>}>
           <Route index element = {<Water/>}/>
