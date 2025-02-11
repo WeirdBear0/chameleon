88d386cd2065878443e6c7b5a42629deb6ca9d16
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-02-10 18:33:25 -0800

    partners

--- src/home-components/partners.js
+++ src/home-components/partners.js
@@ -2,14 +2,16 @@ import React, {useState} from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './partners.css'
 import republic from "./republic.jpg"
+import give from "./msft_give.png"
+import fm from "./future-martians.webp"
 
 const Partners = () => {
   return (
     <div className='partners'>
-        <p className='partnerHeader'>Our Partners</p>
-        <div className='imgContainer'>
-            <img className = "republic" src = {republic}></img>
-        </div>
+        <p className='partnerHeader'>Proudly Partnered With:</p>
+        <img className = "republic" src = {republic}></img>
+        <img className = "give" src = {give}></img>
+        <img className = "fm" src = {fm}></img>
     </div>
 
   );
