84bafa92fe7d7224bba198c2661c808434428ad9
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-05-19 02:37:50 -0700

    mobile fixes

--- src/camp-pages/ecology.js
+++ src/camp-pages/ecology.js
@@ -8,11 +8,8 @@ function Ecology() {
         <div className='info-container'>
           <div className='info-width'>
             <p className='info'>
-              Our Ecology and Technology project focuses on the exploration of advanced farming through small-scale class farms integrated 
-              with analytic devices such as the Microsoft FarmBeat. Students will explore this large project throughout the week and will
-              accompany it with a mini-project involving the creation of a terrarium. Additionally, students will take a field trip to
-              Bellevue Botanical Garden to understand and explore various niches in global environments. To finish off, presentations
-              and class discussions will be held to encapsulate the week's learning!
+            The Ecology and Technology project involves students exploring advanced farming using small-scale class farms with tools like Microsoft FarmBeat. Throughout the week, students will also create terrariums and visit the Bellevue Botanical Garden to study global ecosystems. The week concludes with presentations and class discussions to reflect on their learning.
+
             </p>
           </div>
           
--- src/camp-pages/water.js
+++ src/camp-pages/water.js
@@ -8,12 +8,8 @@ function Water() {
         <div className='info-container'>
           <div className='info-width'>
             <p className='info'>
-              In our Water Project, students will begin learning about the water cycle and fundamental concepts that affect how water interacts with
-              the environment around it. As students dive into the project, they will be presented with a diorama featuring a prominent water feature (river, lake, waterfall)
-              and objects such as mountains, villages, and forests. The problem students will aim to solve will be to prevent the village from being destroyed from the 
-              prominent water feature. Concepts regarding engineering and environmental science will be implemented to protect the village. Midweek, students will
-              embark on a community service project, where they will clean up a nearby creek or stream. Finally, real-life examples of the dioramas will be introduced, and
-              case studies will be conducted to make strong connections with the real world. 
+            In the Water Project, students will explore the water cycle and its environmental impact through a hands-on diorama challenge focused on protecting a village from a major water feature. They’ll apply engineering and environmental science concepts, participate in a creek cleanup, and examine real-life case studies to connect their learning to real-world scenarios!
+
             </p>
           </div>
           
