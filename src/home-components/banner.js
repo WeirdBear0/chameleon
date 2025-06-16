c23feb7cb376e9b6df769c397a961abb68abd70d
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-06-16 16:47:35 -0700

    cool banner

--- src/home-components/banner.js
+++ src/home-components/banner.js
@@ -1,18 +1,21 @@
 import React, {useState} from 'react';
 import './banner.css'
 import Donate  from './donate'
+import GradientBanner from './gradientbanner';
 
 const Banner = () => {
   return (
-    <div className = 'banner'>
-      <div className='title-container'>
-        <p className='title'>A Breath of Fresh Air</p>
-        <p className='description'>Innovative camps with hands-on learning</p>
-        <p className='specs'> EST. 2024 | Sammamish, WA </p>
-        <br></br>
-        <span className='donate-banner'><Donate/></span>
-      </div>
-    </div>
+      <GradientBanner>
+        <div className = 'banner'>
+          <div className='title-container'>
+            <p className='title'>A Breath of Fresh Air</p>
+            <p className='description'>Innovative camps with hands-on learning</p>
+            <p className='specs'> EST. 2024 | Sammamish, WA </p>
+            <br></br>
+            <span className='donate-banner'><Donate/></span>
+          </div>
+        </div>
+      </GradientBanner>
   );
 };
 export default Banner;
