e93ac5fa5ad1d0e3da933b9784f32c377a79eaaf
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-07-17 23:21:32 -0700

    fixing  navbar

--- src/external-nav/windmill.js
+++ src/external-nav/windmill.js
@@ -53,40 +53,33 @@ void loop() {
 }`;
 
 function Windmill() {
-  const [size, setSize] = useState('55px');
-  const [opacity, setOpacity] = useState(1);
   const footerRef = useRef(0);
-
-  function changeNav() {
-    if (window.scrollY > 80) {
-      setSize('34px');
-      setOpacity(0.85);
-    } else {
-      setSize('55px');
-      setOpacity(1);
-    }
-  }
-  window.onscroll = () => {
-    changeNav();
-  };
 
   return (
     <div className="App">
       <Font />
-      <div className={styles.navbar} style={{ background: `rgba(226, 218, 201, ${opacity})` }}>
-        <div className={styles.logoContainer}>
-          <Link to="/">
-            <a href="#" className={styles.logoLink}>
-              <img src={logo} className={styles.logo} alt="chameleon" />
-              <img src={circleLogo} className={styles.mobLogo} alt="chameleon" />
-            </a>
-          </Link>
+      <div className="navbar">
+        <div className="navbar-main">
+          <div className="logoContainer">
+            <Link to="/">
+              <a href="#" className="logoLink">
+                <img src={logo} className="logo" alt="chameleon" />
+                <img src={circleLogo} className="mobLogo" alt="chameleon" />
+              </a>
+            </Link>
+          </div>
+          <div className="title-container">
+            <p className="title" style={{ fontSize: '40px' }}>chameleon</p>
+          </div>
+          <div className="mob-donate" style={{ display: 'none' }}></div>
+          <div className="links">
+            <Link to="/">
+              <a className="sectionLink">Home</a>
+            </Link>
+          </div>
         </div>
-        <p className={styles.title} style={{ fontSize: size }}>chameleon</p>
-        <div className={styles.links}>
-          <Link to="/">
-            <a>Home</a>
-          </Link>
+        <div className="navbar-announcement">
+          {/* Optionally add a signup button or announcement here */}
         </div>
       </div>
       <div className={styles.windmillContent}>
