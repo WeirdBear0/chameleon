d59904c7ed234a7252b0df7240fcdff96b2db7c2
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-07-23 23:18:19 -0700

    delete old ws

--- src/signup.js
+++ src/signup.js
@@ -141,7 +141,6 @@ function SignUp() {
                 required
               >
                 <option value="">Select a workshop</option>
-                <option value="july 8">July 2, 2025 1-4 PM - Turbine Workshop</option>
                 <option value="august 1">August 1, 2025 1-5 PM - Smart Farming Workshop</option>
               </select>
             </div>
