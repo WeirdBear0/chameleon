c8aa0c4e1f0aeeb43ec8f9f33a683d06a3a7d525
weirdbear0 <ayushagarwalsamm@gmail.com>
2025-07-29 16:29:56 -0700

    fix

--- src/home-components/camps.js
+++ src/home-components/camps.js
@@ -68,7 +68,7 @@ const Camps = () => {
   // Workshop details for display
   // To edit camp/workshop info, update the workshopDetails object below:
   const workshopDetails = {
-    'august 1': {
+    'august 4': {
       date: 'August 4, 2025, 1:30-5 PM',
       location: 'Sammamish Library -  825 228th Ave SE, Sammamish, WA',
       title: 'Smart Farming Workshop',
@@ -106,7 +106,7 @@ const Camps = () => {
               onChange={e => setSelectedWorkshop(e.target.value)}
             >
               <option value="">select a workshop</option>
-              <option value="august 1">August 1, 2025 1-5 PM - Smart Farming Workshop</option>
+              <option value="august 1">August 4, 2025 1:30-5 PM - Smart Farming Workshop</option>
             </select>
           </div>
         </div>
