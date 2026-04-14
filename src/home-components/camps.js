9c17503e746d650c667c057193e9a54a3d056012
weirdbear0 <ayushagarwalsamm@gmail.com>
2026-04-14 00:30:31 -0700

    cleaned comments

--- src/home-components/camps.js
+++ src/home-components/camps.js
@@ -48,17 +48,6 @@ const Camps = () => {
   }, [isInView]);
 
   return (
-
-    // <div style={{ height: '600px', position: 'relative' }}>
-    //   <Carousel
-    //     baseWidth={300}
-    //     autoplay={true}
-    //     autoplayDelay={3000}
-    //     pauseOnHover={true}
-    //     loop={true}
-    //     round={false}
-    //   />
-    // </div>
     <div className='ovr'>
       <div className='campsContainer'>
         <div className="sectionHeader">
